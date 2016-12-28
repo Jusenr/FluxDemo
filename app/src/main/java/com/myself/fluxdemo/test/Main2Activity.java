05317a11110c73a418045a3150eeb747292658a9
Jusenr <jusenr@163.com>
2016-12-29 00:13:11 +0800

    first update

---
+++ app/src/main/java/com/myself/fluxdemo/test/Main2Activity.java
@@ -0,0 +1,15 @@
+package com.myself.fluxdemo.test;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.myself.fluxdemo.R;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
