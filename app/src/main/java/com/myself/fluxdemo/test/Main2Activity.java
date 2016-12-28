6366fcebc51caf9352a83d77c934e193c70770d6
Jusenr <jusenr@163.com>
2016-12-29 00:24:54 +0800

    创建分支

--- app/src/main/java/com/myself/fluxdemo/test/Main2Activity.java
+++ app/src/main/java/com/myself/fluxdemo/test/Main2Activity.java
@@ -1,15 +1,37 @@
 package com.myself.fluxdemo.test;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.myself.fluxdemo.R;
+import com.myself.fluxdemo.basic.PTWDActivity;
 
-public class Main2Activity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.OnClick;
+
+public class Main2Activity extends PTWDActivity {
+
+    @BindView(R.id.tv_text)
+    TextView mTvText;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+    protected int getLayoutId() {
+        return R.layout.activity_main2;
+    }
+
+    @Override
+    protected void onViewCreatedFinish(Bundle saveInstanceState) {
+        addNavigation();
+    }
+
+    @Override
+    protected String[] getRequestUrls() {
+        return new String[0];
+    }
+
+
+    @OnClick(R.id.tv_text)
+    public void onClick() {
+        mTvText.setText("你好！");
     }
 }
